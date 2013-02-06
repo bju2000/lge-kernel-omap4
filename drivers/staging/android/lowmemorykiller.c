2b8814a564473fc409c741c46f71e28dc30324dc
faux123 <reioux@gmail.com>
2013-02-06 11:56:24 -0600

    android/lowmemorykiller: Wait for memory to be freed
    The memory reclaim code needs to give time to the system to
    return the memory from a killed process otherwise the memory
    reclaim code could run continuously, in multiple threads,
    which could starve both the watchdog thread and the thread
    which is responsible for returning the memory from the
    killed process.
    
    Change-Id: Ieded4bfe038ca936247fa4b638070e979b02eaa1
    CRs-fixed: 447740
    Signed-off-by: Liam Mark <lmark@codeaurora.org>
    modified for Mako from CAF reference
    
    Signed-off-by: faux123 <reioux@gmail.com>
    
    Conflicts:
    	drivers/staging/android/lowmemorykiller.c
    
    Conflicts:
    
    	drivers/staging/android/lowmemorykiller.c
    
    Conflicts:
    	drivers/staging/android/lowmemorykiller.c

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -35,6 +35,9 @@
 #include <linux/oom.h>
 #include <linux/sched.h>
 #include <linux/notifier.h>
+#include <linux/compaction.h>
+#include <linux/mutex.h>
+#include <linux/delay.h>
 
 //<!-- BEGIN: hyeongseok.kim@lge.com 2012-08-16 -->
 //<!-- MOD : make LMK see swap condition 
@@ -79,23 +82,7 @@ static unsigned long lowmem_deathpending_timeout;
 			printk(x);			\
 	} while (0)
 
-static int
-task_notify_func(struct notifier_block *self, unsigned long val, void *data);
-
-static struct notifier_block task_nb = {
-	.notifier_call	= task_notify_func,
-};
-
-static int
-task_notify_func(struct notifier_block *self, unsigned long val, void *data)
-{
-	struct task_struct *task = data;
-
-	if (task == lowmem_deathpending)
-		lowmem_deathpending = NULL;
-
-	return NOTIFY_OK;
-}
+static DEFINE_MUTEX(scan_mutex);
 
 static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 {
@@ -108,8 +95,17 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	int selected_tasksize = 0;
 	int selected_oom_adj;
 	int array_size = ARRAY_SIZE(lowmem_adj);
-	int other_free = global_page_state(NR_FREE_PAGES);
-	int other_file = global_page_state(NR_FILE_PAGES) -
+	int other_free;
+	int other_file;
+	unsigned long nr_to_scan = sc->nr_to_scan;
+
+	if (nr_to_scan > 0) {
+		if (mutex_lock_interruptible(&scan_mutex) < 0)
+			return 0;
+	}
+
+	other_free = global_page_state(NR_FREE_PAGES);
+	other_file = global_page_state(NR_FILE_PAGES) -
 						global_page_state(NR_SHMEM);
 
 //<!-- BEGIN: hyeongseok.kim@lge.com 2012-08-16 -->
@@ -161,17 +157,21 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 			break;
 		}
 	}
-	if (sc->nr_to_scan > 0)
+	if (nr_to_scan > 0)
 		lowmem_print(3, "lowmem_shrink %lu, %x, ofree %d %d, ma %d\n",
-			     sc->nr_to_scan, sc->gfp_mask, other_free, other_file,
-			     min_adj);
+				nr_to_scan, sc->gfp_mask, other_free,
+				other_file, min_score_adj);
 	rem = global_page_state(NR_ACTIVE_ANON) +
 		global_page_state(NR_ACTIVE_FILE) +
 		global_page_state(NR_INACTIVE_ANON) +
 		global_page_state(NR_INACTIVE_FILE);
-	if (sc->nr_to_scan <= 0 || min_adj == OOM_ADJUST_MAX + 1) {
+	if (nr_to_scan <= 0 || min_score_adj == OOM_SCORE_ADJ_MAX + 1) {
 		lowmem_print(5, "lowmem_shrink %lu, %x, return %d\n",
-			     sc->nr_to_scan, sc->gfp_mask, rem);
+			     nr_to_scan, sc->gfp_mask, rem);
+
+		if (nr_to_scan > 0)
+			mutex_unlock(&scan_mutex);
+
 		return rem;
 	}
 	selected_oom_adj = min_adj;
@@ -193,7 +193,11 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		sig = p->signal;
 		if (!mm || !sig) {
 			task_unlock(p);
-			continue;
+			rcu_read_unlock();
+			/* give the system time to free up the memory */
+			msleep_interruptible(20);
+			mutex_unlock(&scan_mutex);
+			return 0;
 		}
 		oom_adj = sig->oom_adj;
 		if (oom_adj < min_adj) {
@@ -235,10 +239,19 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		lowmem_deathpending_timeout = jiffies + HZ;
 		force_sig(SIGKILL, selected);
 		rem -= selected_tasksize;
+		/* give the system time to free up the memory */
+		msleep_interruptible(20);
 	}
 	lowmem_print(4, "lowmem_shrink %lu, %x, return %d\n",
-		     sc->nr_to_scan, sc->gfp_mask, rem);
-	read_unlock(&tasklist_lock);
+		     nr_to_scan, sc->gfp_mask, rem);
+
+	rcu_read_unlock();
+
+	if (selected)
+        	compact_nodes(false);
+
+	mutex_unlock(&scan_mutex);
+	
 	return rem;
 }
 

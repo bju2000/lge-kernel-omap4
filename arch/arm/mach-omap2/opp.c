9eccdf5e940db291642377f9c44b56bc29654903
TobiasBaeumer@gmail.com <TobiasBaeumer@gmail.com>
2013-03-08 20:12:39 +0100

    Fix for last merge

--- arch/arm/mach-omap2/opp.c
+++ arch/arm/mach-omap2/opp.c
@@ -112,6 +112,8 @@ int __init omap_init_opp_table(struct omap_opp_def *opp_def,
 				dev_err(dev, "%s:%s:err dvfs register %d %d\n",
 					__func__, opp_def->hwmod_name, r, i);
 		}
+next:
+		opp_def++;
 	}
 
 	return 0;
